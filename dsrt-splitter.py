46e46ad224ca4b0d2ec5b878ebc9823303c0bd1d
Dhyandra Raka <78241774+andraaaaa@users.noreply.github.com>
2022-05-29 21:41:48 +0800

    Update dsrt-splitter.py

--- dsrt-splitter.py
+++ dsrt-splitter.py
@@ -5,6 +5,13 @@ from PIL import Image
 from pdfminer.high_level import extract_text
 from pdfminer.pdfparser import PDFParser
 
+# VARIABEL INI DIGUNAKAN SEBAGAI PEMBENTUK PAGE BREAK ANTAR FILE.
+# Bisa diganti tergantung kebutuhan dan gambar target
+# Untuk menggunakan silakan cek pada fungsi get_pics()
+
+br = (100, 100, 8) #Logo Burung Garuda
+
+# ---------------------------------------------------------------------
 # PDF Splitter dengan rentang page
 def split_pdf(file_origin, new_file, pg_start, pg_end):
     pdf_file = PdfFileReader(open(file_origin, "rb"))
@@ -22,30 +29,7 @@ def split_pdf(file_origin, new_file, pg_start, pg_end):
 
     return pdf_new
 
-# Get image overview from page break candidate
-def get_pics(p, pg):
-    filepdf = fitz.open(p)
-    page = filepdf[pg-1]
-    image_list = page.getImageList()
-    # printing number of images found in this page
-    if image_list:
-        print(f"[+] Found a total of {len(image_list)} images in page {pg}")
-    else:
-        print("[!] No images found on page", pg)
-    for image_index, img in enumerate(page.getImageList(), start=1):
-        # get the XREF of the image
-        xref = img[0]
-        # extract the image bytes
-        base_image = filepdf.extractImage(xref)
-        image_bytes = base_image["image"]
-        # get the image extension
-        image_ext = base_image["ext"]
-        # load it to PIL
-        image = Image.open(io.BytesIO(image_bytes))
-        # save it to local disk
-        image.save(open(f"image{pg}_{image_index}.{image_ext}", "wb"))
-
-def add_bs_loc_info(p, pg):
+def add_bs_loc_info_v1(p, pg):
     with fitz.open(p) as pdf_doc:
         text = ''
         pg_spot = pdf_doc.loadPage(pg)
@@ -56,7 +40,23 @@ def add_bs_loc_info(p, pg):
         elim = tx[11].replace('/', ' ')
         outp = [tx[10], elim, nks]
         return outp
+# ------------------------------------------
+# UNTUK DSRT
+# Untuk PPLF, index array sudah disesuaikan. Silakan diganti indexnya dengan
+# menggunakan fungsi cek_info()
 
+def add_bs_dsrt(p, pg):
+    a = cek_info(p, pg)
+    outp = [clean_text(a[11]), clean_text(a[14]), clean_text(a[21])]
+    return outp
+
+# Untuk nama wilayah yang menggunakan garis miring, diganti strip menghindari kesalahan rename file
+def clean_text(t):
+    t = t.replace('/', '-')
+    t = re.sub(' +', ' ', t)
+    return t
+
+# Untuk memetakan array dari teks PDF. Berguna untuk mengambil elemen nama file yang akan dibuat
 def cek_info(p, pg):
     with fitz.open(p) as pdf_doc:
         text = ''
@@ -65,7 +65,7 @@ def cek_info(p, pg):
         tx = pg_text.split("\n")
         return tx
 
-
+# Ambil satu halaman PDF
 def pick_pdf(file_origin, new_file, pg):
     pdf_file = PdfFileReader(open(file_origin, "rb"))
     #np = pdf_file.getNumPages()
@@ -89,7 +89,30 @@ def convert_pdf(filename, convert_type):
             tx = pg.extractText()
             print(tx)    
 
-br = (228, 194, 8) #Logo Burung Garuda
+# Ambil pratinjau gambar dari PDF halaman tertentu. 
+def get_pics(p, pg):
+    filepdf = fitz.open(p)
+    page = filepdf[pg-1]
+    image_list = page.getImageList()
+    # printing number of images found in this page
+    if image_list:
+        print(f"[+] Found a total of {len(image_list)} images in page {pg}")
+        print(image_list)
+    else:
+        print("[!] No images found on page", pg)
+    for image_index, img in enumerate(page.getImageList(), start=1):
+        # get the XREF of the image
+        xref = img[0]
+        # extract the image bytes
+        base_image = filepdf.extractImage(xref)
+        image_bytes = base_image["image"]
+        # get the image extension
+        image_ext = base_image["ext"]
+        # load it to PIL
+        image = Image.open(io.BytesIO(image_bytes))
+        # save it to local disk
+        image.save(open(f"image{pg}_{image_index}.{image_ext}", "wb"))
+
 # Get Page break list by image exists (Cocok untuk DSRT dan daftar berhalaman depan)
 def getPagebreakList(file_name):
     page_breaks = []
@@ -103,7 +126,9 @@ def getPagebreakList(file_name):
             if (u3, u4, u5) == br: page_breaks.append(i) 
     return page_breaks
 
-def split_pdf_by_page_break(pdf_name, filename_template, dirloc):
+
+# [MAIN LINE] (Updated 5/29 9:37 PM) Membuat daftar dari page break yang sudah ada
+def build_daftar(pdf_name, filename_template, dirloc):
     inputpdf = PdfFileReader(open(pdf_name, "rb"))
     num_pages = inputpdf.numPages
     page_breaks = getPagebreakList(pdf_name)
@@ -117,23 +142,16 @@ def split_pdf_by_page_break(pdf_name, filename_template, dirloc):
             os.makedirs(dirloc)
             print("Folder sudah dibuat.")
             for n in range(0, len(page_breaks)):
-                bs = add_bs_loc_info(pdf_name, page_breaks[n])
+                bs = add_bs_dsrt(pdf_name, page_breaks[n]+1)
                 #if n != len(page_breaks)-1: split_pdf(pdf_name, "%s\\%s_%s_%s.pdf"%(dirloc, filename_template, n+1), page_breaks[n]+1, page_breaks[n+1])
                 if n != len(page_breaks)-1: split_pdf(pdf_name, "%s\\%s_%s_%s_%s.pdf"%(dirloc, filename_template, bs[0], bs[1], bs[2]), page_breaks[n]+1, page_breaks[n+1])
                 #else: split_pdf(pdf_name, "%s\\%s_%s.pdf"%(dirloc, filename_template, n+1), page_breaks[n]+1, num_pages)
                 else: split_pdf(pdf_name, "%s\\%s_%s_%s_%s.pdf"%(dirloc, filename_template, bs[0], bs[1], bs[2]), page_breaks[n]+1, num_pages)
-                print("File %s_%s_%s dibuat."%(bs[0], bs[1], bs[2]))
+                print("File (%d/%d) %s_%s_%s dibuat."%(n+1, len(page_breaks), bs[0], bs[1], bs[2]))
     print("Dokumen PDF berhasil dipecah sebanyak %d dokumen."%(len(page_breaks)))
 
 
-#pdf_name = "lf_sp2020_p_6401_20220412.pdf"
-pdf_name = "F:\_KANTOR 2021\SPLF 2021\PRELIST\lf_sp2020_p_64_20220419_Tahap2\lf_sp2020_p_6404_20220419.pdf"
-pdf_splitted = "splf"
-pdf_outp = "mypdf.pdf"
+ct = "D:\\LFP2022.pdf"
 
-split_pdf_by_page_break(pdf_name, pdf_splitted, "D:\\SPLIT_PRELIST_KUTIM_6")
-#print(getPagebreakList("F:\\_KANTOR 2021\\SPLF 2021\\PRELIST\\lf_sp2020_p_6404_20220412.pdf"))
-#split_pdf(pdf_name, "lf_split_kutim.pdf", 1, 5792)
-#split_pdf(pdf_name, "lf_split_kutim_3.pdf", 8740, 13370)
-#split_pdf(pdf_name, "lf_split_kutim_4.pdf", 13371, 16098)
-#split_pdf(pdf_name, "lf_split_kutim_5.pdf", 16099, 20500)
+#  CONTOH PENGGUNAAN
+build_daftar(ct, "dsrt", "D:\\splitted")
